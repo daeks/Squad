bfcbe9ed0ab7d4222893c0a05a15ab3b5c6996d4
daeks <daeks@users.noreply.github.com>
2020-09-04 23:24:05 +0200

    Update mods.sh

--- mods.sh
+++ mods.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-if [ ! -d "$STEAMWORKSHOPTMP"]; then
+if [ ! -d "$STEAMWORKSHOPTMP" ]; then
   mkdir -p $STEAMWORKSHOPTMP
 fi
 
 if [ -f "$STEAMWORKSHOPCFG" ]; then
   if [ "$MODE" = "INSTALL" ]; then
-    while read -r MODID do
+    while read -r MODID; do
       [[ $MODID =~ ^\/.* ]] && continue
       "${STEAMCMDDIR}/steamcmd.sh" +login anonymous +force_install_dir $STEAMWORKSHOPTMP +workshop_download_item $STEAMWORKSHOPID $MODID +quit
     done < "$STEAMWORKSHOPCFG"
   fi
-else 
+else
   echo "" > $STEAMWORKSHOPCFG
 fi
 
